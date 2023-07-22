a78265e9427f5f6a6885107d01bd465f44944732
issmuth <ismael.mdev@gmail.com>
2023-07-22 21:46:10 +0300

    done

--- _printf.c
+++ _printf.c
@@ -2,4 +2,16 @@
 
 /**
  * _printf - printf replica
- * @format
+ * @format: string to be printed
+ *
+ * Return: the number of characters printed
+ */
+
+int _printf(const char *format, ...)
+{
+	int len;
+
+	len = _strlen(format);
+	write(1, format, len));
+	return(len);
+}
