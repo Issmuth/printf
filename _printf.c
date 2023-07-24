34c0d81535183f0800991800725df8ea59f5bcd5
issmuth <ismael.mdev@gmail.com>
2023-07-24 20:11:40 +0300

    done

---
+++ _printf.c
@@ -0,0 +1,59 @@
+#include "main.h"
+
+/**
+ * _printf - printf replica
+ * @format: string to be printed
+ *
+ * Return: the number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int count = 0;
+	char c;
+	char *s;
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(args, format);
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			switch (*format)
+			{
+				case 'c':
+					c = (char) va_arg(args, int);
+					write(1, &c, 1);
+					count++;
+					break;
+				case 's':
+					s = va_arg(args, char *);
+					if (s == NULL)
+						return (-1);
+
+					while (*s)
+					{
+						write(1, s, 1);
+						s++;
+						count++;
+					}
+					break;
+				default:
+					write(1, format, 1);
+					count++;
+					break;
+			}
+		} else
+		{
+			write(1, format, 1);
+			count++;
+		}
+		format++;
+	}
+
+	va_end(args);
+	return (count);
+}
--- main.c
+++
@@ -1,7 +0,0 @@
-#include "main.h"
-
-int main()
-{
-	_printf("done");
-	return 0;
-}
