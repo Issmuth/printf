112cd91f8ad277de90db5d45c2f82af968e3b0f9
issmuth <ismael.mdev@gmail.com>
2023-07-25 12:06:47 -0700

    done

--- _printf.c
+++ _printf.c
@@ -34,6 +34,8 @@ int _printf(const char *format, ...)
 			/* now check the next char after the special char '%' */
 			specifier = format[i + 1];
 			/* delete the chars */
+			if (print_arg(ap, specifer) == -1)
+				return (-1);
 			j += print_arg(ap, specifier);
 			/* even after there still continue adding the rest of chars available */
 			i++;
